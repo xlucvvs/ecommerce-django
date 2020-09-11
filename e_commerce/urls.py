6eff62fb2fa327362f0ac12178ca1b2a2552c4c7
unknown <lucasribeiro.sec@gmail.com>
2020-09-11 15:19:18 -0300

    servindo arquivos estaticos

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -1,3 +1,6 @@
+from django.conf import settings
+from django.conf.urls.static import static
+
 from django.contrib import admin
 from django.urls import path
 
@@ -11,3 +14,6 @@ urlpatterns = [
     path('register/', register_page),
 	path('admin/', admin.site.urls),
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
