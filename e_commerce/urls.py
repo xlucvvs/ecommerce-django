96eb01406ab96843b568708c77efce110201079a
unknown <lucasribeiro.sec@gmail.com>
2020-09-11 15:33:34 -0300

    criada as list views

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,16 +4,20 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from products.views import ProductListView, product_list_view
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
-	path('', home_page),
-	path('about/', about_page),
-	path('contact/', contact_page),
+    path('', home_page),
+    path('about/', about_page),
+    path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
-	path('admin/', admin.site.urls),
+    path('products/', ProductListView.as_view()),
+    path('products-fbv/', product_list_view),    
+    path('admin/', admin.site.urls),
 ]
+
 if settings.DEBUG:
     urlpatterns = urlpatterns + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,18 @@
+from django.views.generic import ListView
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Product
+
+#Class Based View
+class ProductListView(ListView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all()
+    template_name = "products/list.html"
+
+#Function Based View
+def product_list_view(request):
+    queryset = Product.objects.all()
+    context = {
+        'qs': queryset
+    }
+    return render(request, "products/list.html", context)
