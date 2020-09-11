78999a09e3a07a2cf757314f5e3ab3ac8c27f24c
unknown <lucasribeiro.sec@gmail.com>
2020-09-11 15:55:09 -0300

    criadas detail views

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from products.views import ProductListView, product_list_view
+from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -14,7 +14,9 @@ urlpatterns = [
     path('login/', login_page),
     path('register/', register_page),
     path('products/', ProductListView.as_view()),
-    path('products-fbv/', product_list_view),    
+    path('products-fbv/', product_list_view), 
+    path('products/<int:pk>', ProductDetailView.as_view()),
+    path('products-fbv/<int:pk>', product_detail_view), 
     path('admin/', admin.site.urls),
 ]
 
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 
 from .models import Product
 
@@ -9,7 +9,7 @@ class ProductListView(ListView):
     queryset = Product.objects.all()
     template_name = "products/list.html"
     
-    #def get_context_data(self, *args, **kwargs):
+    #def get_context_data(self):
         #context = super(ProductListView, self).get_context_data(*args, **kwargs)
         #print(context)
         #return context
@@ -21,3 +21,22 @@ def product_list_view(request):
         'object_list': queryset
     }
     return render(request, "products/list.html", context)
+
+#Class Based View
+class ProductDetailView(DetailView):
+    #traz todos os produtos do banco de dados sem filtrar nada 
+    queryset = Product.objects.all()
+    template_name = "products/detail.html"
+    
+    def get_context_data(self, *args, **kwargs):
+        context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
+        print(context)
+        return context
+
+#Function Based View
+def product_detail_view(request, pk = None, *args, **kwargs):
+    instance = get_object_or_404(Product, pk = pk)
+    context = {
+        'object': instance
+    }
+    return render(request, "products/detail.html", context)
