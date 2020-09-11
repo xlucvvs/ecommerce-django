56d48c9ce88969a3cd6a8d7770d66763bbe8db56
unknown <lucasribeiro.sec@gmail.com>
2020-09-11 15:03:43 -0300

    criado formulario de contato

--- e_commerce/views.py
+++ e_commerce/views.py
@@ -1,5 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+
+from .forms import ContactForm
 def home_page(request):
     context = {
         "title": "Página principal",
@@ -15,8 +17,12 @@ def about_page(request):
     return render(request, "about/view.html", context)
 
 def contact_page(request):
+    contact_form  = ContactForm(request.POST or None)
     context = {
         "title": "Página de contato",
-        "content": "Bem-vindo a página de contato"
+        "content": "Bem-vindo a página de contato",
+        "form": contact_form
     }
+    if contact_form.is_valid():
+        print(contact_form.cleaned_data)
     return render(request, "contact/view.html", context)
