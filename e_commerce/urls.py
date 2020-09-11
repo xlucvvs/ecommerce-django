0cc21706d2f9c6fe4ccf056edf488a4be0256d62
unknown <lucasribeiro.sec@gmail.com>
2020-09-11 16:23:41 -0300

    criadas customquery sets

--- e_commerce/urls.py
+++ e_commerce/urls.py
@@ -4,7 +4,14 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
-from products.views import ProductListView, product_list_view, ProductDetailView, product_detail_view
+from products.views import ( 
+    ProductListView, 
+    product_list_view, 
+    ProductDetailView, 
+    product_detail_view, 
+    ProductFeaturedListView, 
+    ProductFeaturedDetailView
+)
 from .views import home_page, about_page, contact_page, login_page, register_page
 
 urlpatterns = [
@@ -13,6 +20,8 @@ urlpatterns = [
     path('contact/', contact_page),
     path('login/', login_page),
     path('register/', register_page),
+    path('featured/', ProductFeaturedListView.as_view()),
+    path('featured/<int:pk>/', ProductFeaturedDetailView.as_view()),
     path('products/', ProductListView.as_view()),
     path('products-fbv/', product_list_view), 
     path('products/<int:pk>', ProductDetailView.as_view()),
