74a1364e14615ffc15755b7683315cf4af278fac
unknown <lucasribeiro.sec@gmail.com>
2020-09-11 16:15:44 -0300

    criado custom model managers

--- products/models.py
+++ products/models.py
@@ -1,12 +1,21 @@
 from django.db import models
 
+class ProductManager(models.Manager):
+    def get_by_id(self, id):
+        qs = self.get_queryset().filter(id = id)
+        if qs.count() == 1:
+            return qs.first()
+        return None
+
 # Create your models here.
 class Product(models.Model): #product_category
-    title = models.CharField(max_length=120)
+    title       = models.CharField(max_length=120)
     description = models.TextField()
-    price = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
-    image = models.ImageField(upload_to = 'products/', null = True, blank = True)
+    price       = models.DecimalField(decimal_places=2, max_digits=20, default=100.00)
+    image       = models.FileField(upload_to = 'products/', null = True, blank = True)
 
+    objects = ProductManager()
+    
     #python 3
     def __str__(self):
         return self.title
--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 
@@ -9,11 +10,12 @@ class ProductListView(ListView):
     #traz todos os produtos do banco de dados sem filtrar nada 
     queryset = Product.objects.all()
     template_name = "products/list.html"
-    
-    #def get_context_data(self):
-        #context = super(ProductListView, self).get_context_data(*args, **kwargs)
-        #print(context)
-        #return context
+
+    # def get_context_data(self, *args, **kwargs):
+    #     context = super(ProductListView, self).get_context_data(*args, **kwargs)
+    #     print(context)
+    #     return context
+
 
 #Function Based View
 def product_list_view(request):
@@ -25,22 +27,26 @@ def product_list_view(request):
 
 #Class Based View
 class ProductDetailView(DetailView):
-    #traz todos os produtos do banco de dados sem filtrar nada 
-    queryset = Product.objects.all()
+    #queryset = Product.objects.all()
     template_name = "products/detail.html"
-    
+
     def get_context_data(self, *args, **kwargs):
         context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
         print(context)
         return context
+    
+    def get_object(self, *args, **kwargs):
+        pk = self.kwargs.get('pk')
+        instance = Product.objects.get_by_id(pk)
+        if instance is None:
+            raise Http404("Esse produto não existe!")
+        return instance
 
 #Function Based View
 def product_detail_view(request, pk = None, *args, **kwargs):
-    # instance = get_object_or_404(Product, pk = pk)
-    qs = Product.objects.filter(id = pk)
-    if qs.count() == 1:
-        instance = qs.first()
-    else:
+    instance = Product.objects.get_by_id(pk)
+    print(instance)
+    if instance is None:
         raise Http404("Esse produto não existe!")
 
     context = {
