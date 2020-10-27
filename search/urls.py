741e9025a28613766745b503538f15bd78410d8d
unknown <lucasribeiro.sec@gmail.com>
2020-10-27 19:54:07 -0300

    consulta e pesquisa

--- search/urls.py
+++ search/urls.py
@@ -5,7 +5,6 @@ app_name = "search"
 from .views import (
                         SearchProductView, 
                     )
-
 urlpatterns = [
-    path('', SearchProductView.as_view(), name='list'),
+    path('', SearchProductView.as_view(), name='query'),
 ]
